0b46bcf694f269cdb8be991dc9c395ec11e540c7
Jeffrey Carpenter <jeffrey.carp@gmail.com>
2013-08-06 22:00:28 -0500

    Update bash config for better multi-OS usage

--- install.sh
+++ install.sh
@@ -1,8 +1,22 @@
 #!/bin/sh
 
 WORKING_DIR=$(pwd)
+MKDIR_COMMAND=$(which mkdir)
 LINK_COMMAND=$(which ln)
 
+# vim configuration
+${MKDIR_COMMAND} -p ${HOME}/.vim
+${MKDIR_COMMAND} -p ${HOME}/.vim/backup
+${MKDIR_COMMAND} -p ${HOME}/.vim/tmp
+${LINK_COMMAND} -sf ${WORKING_DIR}/vim/autoload $HOME/.vim/autoload
+${LINK_COMMAND} -sf ${WORKING_DIR}/vim/bundle $HOME/.vim/bundle
+${LINK_COMMAND} -sf ${WORKING_DIR}/vim/colors $HOME/.vim/colors
+${LINK_COMMAND} -sf ${WORKING_DIR}/vim/vimrc $HOME/.vimrc
+
+# Bash supporting configuration
+${LINK_COMMAND} -sf ${WORKING_DIR}/colors $HOME/.colors
+
+# Bash scripts
 ${LINK_COMMAND} -sf ${WORKING_DIR}/bash/bash_aliases $HOME/.bash_aliases
 ${LINK_COMMAND} -sf ${WORKING_DIR}/bash/bash_cflags $HOME/.bash_cflags
 ${LINK_COMMAND} -sf ${WORKING_DIR}/bash/bash_login $HOME/.bash_login
