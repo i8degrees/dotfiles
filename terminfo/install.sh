b04325f142f0bcd98f14a8514405976da30cd6e3
Jeffrey Carpenter <i8degrees@gmail.com>
2021-12-13 23:13:01 -0600

    Add install script

---
+++ terminfo/install.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+PREFIX=/usr/share/terminfo # $HOME/.terminfo
+TIC_BIN=$(command -v tic) # /usr/bin/tic
+
+if [ ! -x "$TIC_BIN" ]; then
+  echo "CRITICAL: The termcap compiler -- tic -- was not found."
+  echo
+  printf "\t"
+  echo "sudo apt install ncurses-bin"
+  echo
+  exit 255
+fi
+
+if [ ! -d "$PREFIX" ]; then
+  echo "WARNING: The selected prefix path at $PREFIX does not exist."
+  echo
+  echo "Installing to the default prefix at $HOME."
+  PREFIX="$HOME/.terminfo"
+fi
+
+sudo tic iterm.terminfo -o $PREFIX
+sudo tic screen-256color-italic.terminfo -o $PREFIX
+sudo tic tmux-256color.terminfo -o $PREFIX
+sudo tic tmux.terminfo -o $PREFIX
+
