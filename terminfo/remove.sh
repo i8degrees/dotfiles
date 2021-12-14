2dda79ad8080bbb3b960518bbc9cee2add96fa2c
Jeffrey Carpenter <i8degrees@gmail.com>
2021-12-13 23:13:18 -0600

    Add remove script

---
+++ terminfo/remove.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#!/bin/sh
+
+#sudo rm -fv /usr/share/terminfo/i/iterm.terminfo
+sudo rm -fv /usr/share/terminfo/i/iterm*
+sudo rm -fv /usr/share/terminfo/i/iTerm*
+sudo rm -fv /usr/share/terminfo/s/screen-256color-italic.terminfo
+sudo rm -fv /usr/share/terminfo/t/tmux-256color.terminfo
+sudo rm -fv /usr/share/terminfo/t/tmux.terminfo
+
