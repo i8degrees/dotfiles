d8b5a51d60ace085a67bbd5c32f55159556a362b
Jeffrey Carpenter <1329364+i8degrees@users.noreply.github.com>
2024-03-18 21:24:16 -0500

    Add ssh domain config
    + Add key bindings to attempt the redirection of Application
    close events to NO OP.

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -19,6 +19,56 @@ config.colors = {
   scrollbar_thumb = 'pink'
 }
 
+-- NOTE(jeff): Use wezterm.action.DisableDefaultAssignment in order to pass
+-- the key action to the terminal instead of acting upon it.
+--
+-- 1. wezterm show-keys --lua
+-- 2. https://wezfurlong.org/wezterm/config/default-keys.html
+config.keys = {
+  {
+    key = 'q',
+    mods = 'CTRL',
+    action = wezterm.action.Nop
+    --action = wezterm.action.QuitApplication
+  },
+  {
+    key = 'q',
+    mods = 'SUPER',
+    action = wezterm.action.Nop
+    --action = wezterm.action.QuitApplication
+  },
+  {
+    key = 'q',
+    mods = 'ALT',
+    action = wezterm.action.Nop
+    --action = wezterm.action.QuitApplication
+  },
+  {
+    key = 'h',
+    mods = 'SUPER',
+    action = wezterm.action.Nop
+    --action = wezterm.action.Hide
+  },
+  {
+    key = 'w',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.Nop
+    --action = wezterm.action.CloseCurrentPane { confirm = true },
+  },
+  {
+    key = 'w',
+    mods = 'CTRL',
+    action = wezterm.action.Nop
+    --action = wezterm.action.CloseCurrentPane { confirm = true },
+  },
+  {
+    key = 'w',
+    mods = 'SUPER',
+    action = wezterm.action.Nop
+    --action = wezterm.action.CloseCurrentTab { confirm = true },
+  },
+}
+
 -- https://wezfurlong.org/wezterm/config/lua/config/background.html?h=background#source-definition
 config.window_background_image = HOME .. "/Pictures/wallpapers/anita.island_12407688_530562150457048_1519659826_n.jpg"
 config.window_background_image_hsb = {
@@ -27,5 +77,44 @@ config.window_background_image_hsb = {
   saturation = 1.0,
 }
 --wezterm.log_info("BG Image: " .. config.window_background_image);
+
+-- https://wezfurlong.org/wezterm/multiplexing.html
+config.ssh_domains = {
+  {
+    name = 'ns3.home',
+    remote_address = 'ns3.home',
+    username = 'root',
+  },
+  {
+    name = 'ns4.home',
+    remote_address = 'ns4.home',
+    username = 'root',
+  },
+  {
+    name = 'fs1.home',
+    remote_address = 'fs1.home',
+    username = 'jeff',
+  },
+  {
+    name = 'scorpio.home',
+    remote_address = 'scorpio.home',
+    username = 'jeff',
+  },
+  {
+    name = 'scorpio-pve.home',
+    remote_address = 'scorpio-pve.home',
+    username = 'root',
+  },
+  {
+    name = 'virgo.home',
+    remote_address = 'virgo.home',
+    username = 'jeff',
+  },
+  {
+    name = 'virgo-wifi.home',
+    remote_address = 'virgo-wifi.home',
+    username = 'jeff',
+  },
+}
 
 return config
