53e436745b27d3be2a3ff3f09c2d145a32ed5595
Jeffrey Carpenter <1329364+i8degrees@users.noreply.github.com>
2024-07-21 00:57:33 -0500

    Variable refactoring

--- proxmox-backup-client/local/src/tests/test1.sh
+++ proxmox-backup-client/local/src/tests/test1.sh
@@ -10,14 +10,12 @@ set -o errexit
 source "$HOME/local/lib/pve_util.sh"
 source "$HOME/.config/proxmox-backup/pbs1.password"
 
-ROOT_INCLUDES="--include-dev /boot --include-dev /boot/efi --include-dev /efi --include-dev /opt --include-dev /opt/bin --include-dev /opt/sbin --include-dev /opt/go --include-dev /opt/python3 --include-dev /opt/share --include-dev /usr/local/src --include-dev /var/lib"
-
-TEST1="/boot /boot/efi /efi /opt /opt/bin /opt/sbin /opt/go /opt/python3 /opt/share /usr/local/src /var/lib"
-
 echo from_proxmox_include
-from_proxmox_include "$ROOT_INCLUDES"
+from_proxmox_include "--include-dev /boot --include-dev /boot/efi --include-dev /efi --include-dev /opt --include-dev /opt/bin --include-dev /opt/sbin --include-dev /opt/go --include-dev /opt/python3 --include-dev /opt/share --include-dev /usr/local/src --include-dev /var/lib"
 echo
 
 echo to_proxmox_include
-to_proxmox_include $TEST1
+# to_proxmox_include $TEST1
+ret=$(to_proxmox_include /boot /boot/efi /efi /opt /opt/bin /opt/sbin /opt/go /opt/python3 /opt/share /usr/local/src /var/lib)
+echo $ret
 echo
