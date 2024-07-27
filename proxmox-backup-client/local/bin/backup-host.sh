21500243463d5c3fdf8c3ff905aaada382eda5c9
Jeffrey Carpenter <1329364+i8degrees@users.noreply.github.com>
2024-07-27 17:20:44 -0500

    Further cleanup
    + Linter notes

--- proxmox-backup-client/local/bin/backup-host.sh
+++ proxmox-backup-client/local/bin/backup-host.sh
@@ -119,10 +119,9 @@ INCLUDES=""
 # IMPORTANT(JEFF): The following two variables can also be referred to from
 # their respective file paths as commented below.
 # ~/.config/proxmox-backup/pbs1.password
-DEFAULT_ROOT_INCLUDES=$(to_proxmox_include /boot /boot/efi /efi /opt /opt/bin /opt/sbin /opt/go /opt/python3 /opt/share /usr/local/bin /usr/local/src /var/lib /var/log)
-DEFAULT_HOME_INCLUDES=$(to_proxmox_include /home /home/jeff /home/api /home/scripts)
-# shellcheck disable=SC2034
-DEFAULT_EXCLUSIONS=$(to_proxmox_exclude /dev/ /mnt/ /net/ /run/ /sys/ /tmp/ /cifs/ /misc/ /proc/ /media/ /usr/src/ /var/log/ /var/run/ /timeshift /var/cache /var/lock/ /var/mail/ /var/spool/ /home/recoll/ /home/timeshift/ /var/cache/man/ /var/tmp/pamac/ /var/cache/pamac/ /var/cache/pacman/ /var/cache/rclone/ /var/cache/fscache/ /var/cache/pkgfile/ /var/cache/private/ /var/cache/ /var/lib/systemd/coredump /var/tmp/pamac-build-jeff/ /dev /etc/mtab /home/test/.cache/ /media /mnt /proc /run /sys /timeshift /var/cache/ /var/crash /var/lib/flatpak /var/lock /var/log /var/run /var/spool /var/tmp /lost+found *.cache* *node_modules* /home/linuxbrew/ /home/test /home/jeff/Backups/borg.json /home/jeff/Backups/virgo.lan /home/jeff/Backups/scorpio /home/jeff/Backups/fs1 /home/jeff/Projects/sunshine_t1_elite /home/jeff/Projects/syn-net/ /home/jeff/.local/share/akonadi/ /home/jeff/.local/share/docker/ /home/jeff/.local/share/fsearch /home/jeff/.local/share/NuGet/ /home/jeff/.local/share/baloo/ /home/jeff/Videos/pr0n/ /home/jeff/.docker/desktop/vms /home/jeff/.docker/desktop/log /home/jeff/Software/ /home/jeff/.local/share/Trash/ /root/.cache/ /root/.local/share/Trash /Cloud *Downloads* /var/cache/private/ /var/tmp/rclone/ /home/jeff/.config/google-chrome /home/jeff/.config/android-messages-desktop /home/jeff/.config/Bitwarden .android .audacity-data .cache .cargo .cddb .config/chromium .julia .local/bin .local/share/baloo .local/share/Steam .local/share/Trash .npm .pki .steam *.socket* .Xauthority .steampid)
+DEFAULT_ROOT_INCLUDES=$(to_proxmox_include "/boot /boot/efi /efi /opt /opt/bin /opt/sbin /opt/go /opt/python3 /opt/share /usr/local/bin /usr/local/src /var/lib /var/log")
+DEFAULT_HOME_INCLUDES=$(to_proxmox_include "/home /home/jeff /home/api /home/scripts")
+DEFAULT_EXCLUSIONS=$(to_proxmox_exclude "/dev/ /mnt/ /net/ /run/ /sys/ /tmp/ /cifs/ /misc/ /proc/ /media/ /usr/src/ /var/log/ /var/run/ /timeshift /var/cache /var/lock/ /var/mail/ /var/spool/ /home/recoll/ /home/timeshift/ /var/cache/man/ /var/tmp/pamac/ /var/cache/pamac/ /var/cache/pacman/ /var/cache/rclone/ /var/cache/fscache/ /var/cache/pkgfile/ /var/cache/private/ /var/cache/ /var/lib/systemd/coredump /var/tmp/pamac-build-jeff/ /dev /etc/mtab /home/test/.cache/ /media /mnt /proc /run /sys /timeshift /var/cache/ /var/crash /var/lib/flatpak /var/lock /var/log /var/run /var/spool /var/tmp /lost+found *.cache* *node_modules* /home/linuxbrew/ /home/test /home/jeff/Backups/borg.json /home/jeff/Backups/virgo.lan /home/jeff/Backups/scorpio /home/jeff/Backups/fs1 /home/jeff/Projects/sunshine_t1_elite /home/jeff/Projects/syn-net/ /home/jeff/.local/share/akonadi/ /home/jeff/.local/share/docker/ /home/jeff/.local/share/fsearch /home/jeff/.local/share/NuGet/ /home/jeff/.local/share/baloo/ /home/jeff/Videos/pr0n/ /home/jeff/.docker/desktop/vms /home/jeff/.docker/desktop/log /home/jeff/Software/ /home/jeff/.local/share/Trash/ /root/.cache/ /root/.local/share/Trash /Cloud *Downloads* /var/cache/private/ /var/tmp/rclone/ /home/jeff/.config/google-chrome /home/jeff/.config/android-messages-desktop /home/jeff/.config/Bitwarden .android .audacity-data .cache .cargo .cddb .config/chromium .julia .local/bin .local/share/baloo .local/share/Steam .local/share/Trash .npm .pki .steam *.socket* .Xauthority .steampid")
 
 if [ "$ARG_HOST" != "" ]; then
   HOST="$ARG_HOST"
@@ -224,21 +223,39 @@ cleanup_passwords
 if [ "$ARG_TYPE" = "system" ]; then
   if [[ -n "$ROOT_INCLUDES" ]] && [[ "$ROOT_INCLUDES" != "" ]]; then
     if echo "$ROOT_INCLUDES | grep -q -i -e '--include-dev'"; then
+      # FIXME(JEFF): Adapt to_proxmox_include function to support quotes in
+      # its argument list?
+      # shellcheck disable=2086
       INCLUDES=$(to_proxmox_include $ROOT_INCLUDES)
     else
+      # FIXME(JEFF): Adapt to_proxmox_include function to support quotes in
+      # its argument list?
+      # shellcheck disable=2086
       INCLUDES=$(from_proxmox_include $ROOT_INCLUDES)
     fi
   else
+    # FIXME(JEFF): Adapt to_proxmox_include function to support quotes in
+    # its argument list?
+    # shellcheck disable=2086
     INCLUDES=$(to_proxmox_include $DEFAULT_ROOT_INCLUDES)
   fi
 elif [ "$ARG_TYPE" = "home" ]; then
   if [[ -n "$HOME_INCLUDES" ]] && [[ "$HOME_INCLUDES" != "" ]]; then
     if echo "$HOME_INCLUDES | grep -q -i -e '--include-dev'"; then
+      # FIXME(JEFF): Adapt to_proxmox_include function to support quotes in
+      # its argument list?
+      # shellcheck disable=2086
       INCLUDES=$(to_proxmox_include $HOME_INCLUDES)
     else
+      # FIXME(JEFF): Adapt to_proxmox_include function to support quotes in
+      # its argument list?
+      # shellcheck disable=2086
       INCLUDES=$(from_proxmox_include $HOME_INCLUDES)
     fi
   else
+    # FIXME(JEFF): Adapt to_proxmox_include function to support quotes in
+    # its argument list?
+    # shellcheck disable=2086
     INCLUDES=$(to_proxmox_include $DEFAULT_HOME_INCLUDES)
   fi
 fi
@@ -251,19 +268,23 @@ EXTRA_ARGS=()
 if [ "$ARG_TYPE" = "system" ]; then
   # rootfs dirs
   if [ -e "/.pxarexclude" ]; then
+    # shellcheck disable=2206
     EXTRA_ARGS+=("${HOST}_root.pxar:/" $INCLUDES)
     echo "INFO: Using exclusion list at /.pxarexclude"
   else
-    EXTRA_ARGS+=("${HOST}_root.pxar:/" $EXCLUSIONS_LIST $INCLUDES")
+    # shellcheck disable=2206
+    EXTRA_ARGS+=("${HOST}_root.pxar:/" $EXCLUSIONS_LIST $INCLUDES)
     echo "INFO: Using exclusion list from $PASSFILE"
   fi
 elif [ "$ARG_TYPE" = "home" ]; then
   # home dir (user)
   if [ -e "$HOME/.pxarexclude" ]; then
-    EXTRA_ARGS+=("${HOST}_home.pxar:/home" "$INCLUDES")
+    # shellcheck disable=2206
+    EXTRA_ARGS+=("${HOST}_home.pxar:/home" $INCLUDES)
     echo "INFO: Using exclusion list at $HOME/.pxarexclude"
   else # .config/proxmox-backup/pbs1
-    EXTRA_ARGS+=("${HOST}_home.pxar:/home" $EXCLUSIONS_LIST" "$INCLUDES")
+    # shellcheck disable=2206
+    EXTRA_ARGS+=("${HOST}_home.pxar:/home" $EXCLUSIONS_LIST $INCLUDES)
     echo "INFO: Using exclusion list from $PASSFILE"
   fi # if [ -e "$HOME/.pxarexclude" ]; then
 fi # system
@@ -272,7 +293,7 @@ if [ -n "$NAMESPACE" ]; then
   EXTRA_ARGS+=("--ns" "$NAMESPACE")
 fi
 
-[ -n "$DEBUG" ] && echo -e "proxmox-backup-client backup ${EXTRA_ARGS}\n"
+[ -n "$DEBUG" ] && echo -e "proxmox-backup-client backup ${EXTRA_ARGS[$*]}\n"
 run_cmd proxmox-backup-client backup "${EXTRA_ARGS[@]}"
 
 # example post-hook
