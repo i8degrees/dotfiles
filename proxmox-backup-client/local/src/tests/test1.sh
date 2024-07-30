0a027f61153b01ee45782154c0a197aabf7b187c
Jeffrey Carpenter <1329364+i8degrees@users.noreply.github.com>
2024-07-30 00:51:00 -0500

    Major cleanup of codebase w/ remaining bugs squash

--- proxmox-backup-client/local/src/tests/test1.sh
+++ proxmox-backup-client/local/src/tests/test1.sh
@@ -8,14 +8,26 @@ set -o errexit
 #set -o xtrace
 
 source "$HOME/local/lib/pve_util.sh"
-source "$HOME/.config/proxmox-backup/pbs1.password"
+#source "$HOME/.config/proxmox-backup/pbs1.password"
+
+PROXMOX_INCLUDES="--include-dev /boot --include-dev /boot/efi --include-dev /efi --include-dev /opt --include-dev /opt/bin --include-dev /opt/sbin --include-dev /opt/go --include-dev /opt/python3 --include-dev /opt/share --include-dev /usr/local/src --include-dev /var/lib"
+
+TEST1="/var/cache/apt /var/tmp /var/lock /var/run /var/mail /home/recoll /home/test"
+TEST2="/boot /boot/efi /efi /opt /opt/bin /opt/sbin /opt/go /opt/python3 /opt/share /usr/local/src /var/lib"
 
 echo from_proxmox_include
-from_proxmox_include "--include-dev /boot --include-dev /boot/efi --include-dev /efi --include-dev /opt --include-dev /opt/bin --include-dev /opt/sbin --include-dev /opt/go --include-dev /opt/python3 --include-dev /opt/share --include-dev /usr/local/src --include-dev /var/lib"
+from_proxmox_include $PROXMOX_INCLUDES
 echo
 
 echo to_proxmox_include
-# to_proxmox_include $TEST1
-ret=$(to_proxmox_include /boot /boot/efi /efi /opt /opt/bin /opt/sbin /opt/go /opt/python3 /opt/share /usr/local/src /var/lib)
+ret=$(to_proxmox_include $TEST1)
 echo $ret
 echo
+
+echo parse_inclusions
+INCLUDES=$(parse_inclusions $TEST1)
+echo $INCLUDES
+
+echo "parse_inclusions(null)"
+INCLUDES=$(parse_inclusions )
+echo $INCLUDES
