eaafb5efc6750c53c87b4cf8e124a85b5b0733b0
Jeffrey Carpenter <1329364+i8degrees@users.noreply.github.com>
2024-08-02 12:40:46 -0500

    Update termcap files installer

--- terminfo/install.sh
+++ terminfo/install.sh
@@ -1,7 +1,24 @@
-#!/bin/sh
+#!/usr/bin/env bash
+#
+#
 
-PREFIX=/usr/share/terminfo # $HOME/.terminfo
+INSTALL_BASE="$HOME/dotfiles.git/terminfo"
+PREFIX="$1" # $HOME/.terminfo
+#PREFIX="/usr/share/terminfo" # $HOME/.terminfo
 TIC_BIN=$(command -v tic) # /usr/bin/tic
+
+if [ -z "$PREFIX" ]; then
+  PREFIX="/usr/share/terminfo"
+else
+  PREFIX=$1
+fi
+
+if [ ! -x "$INSTALL_BASE" ]; then
+  echo "CRITICAL: Failed to find installation files..."
+  [ -n "$DEBUG" ] && echo; echo "INSTALL_BASE: ${INSTALL_BASE}"
+  echo
+  exit 2
+fi
 
 if [ ! -x "$TIC_BIN" ]; then
   echo "CRITICAL: The termcap compiler -- tic -- was not found."
@@ -19,8 +36,26 @@ if [ ! -d "$PREFIX" ]; then
   PREFIX="$HOME/.terminfo"
 fi
 
-tic iterm.terminfo -o $PREFIX
-tic screen-256color-italic.terminfo -o $PREFIX
-tic tmux-256color.terminfo -o $PREFIX
-tic tmux.terminfo -o $PREFIX
+[ -n "$DEBUG" ] && echo "PREFIX: $PREFIX"
+
+TERMCAP_FILES=(
+  "${INSTALL_BASE}"/iterm.terminfo
+  "${INSTALL_BASE}"/screen-256color-italic.terminfo
+  "${INSTALL_BASE}"/tmux-256color.terminfo
+  "${INSTALL_BASE}"/tmux.terminfo
+)
+
+for path in "${TERMCAP_FILES[@]}"; do
+  [ -n "$DEBUG" ] && echo "DEBUG: " tic "$path" -o "$PREFIX"
+  tic "$path" -o "$PREFIX"
+done
+
+#tic "${INSTALL_BASE}"/iterm.terminfo -o \
+#  $PREFIX
+#tic "${INSTALL_BASE}"/screen-256color-italic.terminfo -o \
+#  $PREFIX
+#tic "${INSTALL_BASE}"/tmux-256color.terminfo -o \
+#  $PREFIX
+#tic "${INSTALL_BASE}"/tmux.terminfo -o \
+#  $PREFIX
 
