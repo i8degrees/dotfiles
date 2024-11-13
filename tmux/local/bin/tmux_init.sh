d7ee4ff48a2cff05979a163df41e15da7212c30e
Jeffrey Carpenter <1329364+i8degrees@users.noreply.github.com>
2024-11-13 12:38:38 -0600

    Tweak tmux session start

--- tmux/local/bin/tmux_init.sh
+++ tmux/local/bin/tmux_init.sh
@@ -5,7 +5,7 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 USE_TMUX=1
 
 if [ -n "$USE_TMUX" ]; then
-  tmux attach || tmux
+  tmux attach || tmux new -s default -A
 else
   exec /bin/bash -l #-c 'cd ~/Notes.git'
 fi
