7de8c09a1f0bdc8424f2567d8f6a5ff62170a4f6
Jeffrey Carpenter <1329364+i8degrees@users.noreply.github.com>
2024-11-13 12:38:46 -0600

    Add check_os function and stub out most of the install help texts

--- terminfo/install.sh
+++ terminfo/install.sh
@@ -7,6 +7,24 @@ PREFIX="$1" # $HOME/.terminfo
 #PREFIX="/usr/share/terminfo" # $HOME/.terminfo
 TIC_BIN=$(command -v tic) # /usr/bin/tic
 
+check_os() {
+  result="unknown"
+
+  if [ -z "$ID" ]; then
+    if [ -e "/etc/os-release" ]; then
+      eval "$(cat /etc/os-release)"
+    else
+      echo "$result"
+    fi
+  fi
+
+  if [ -n "$ID" ]; then
+    result="$ID"
+  fi
+
+  echo "$result"
+}
+
 if [ -z "$PREFIX" ]; then
   PREFIX="/usr/share/terminfo"
 else
@@ -23,12 +41,44 @@ fi
 if [ ! -x "$TIC_BIN" ]; then
   echo "CRITICAL: The termcap compiler -- tic -- was not found."
   echo
+  os_stamp=$(check_os)
+  echo $os_stamp
+
+  install_help_text=""
+  case "$os_stamp" in
+    manjaro|arch)
+      # TODO(JEFF): find package names
+      install_help_text="sudo pacman -S ncurses"
+    ;;
+    alpine)
+      install_help_text="apk add ncurses-terminfo"
+    ;;
+    freebsd|netbsd|openbsd)
+      # TODO(JEFF): find package names
+      install_help_text=""
+    ;;
+    darwin)
+      # TODO(JEFF): find package names
+      install_help_text="brew install ncurses"
+    ;;
+    debian|ubuntu)
+      install_help_text="apt install ncurses-bin"
+    ;;
+    windows|cygwin)
+      # TODO(JEFF): find package names
+      install_help_text="brew install ncurses"
+    ;;
+    *)
+      # do nothing default
+    ;;
+  esac
+
   printf "\t"
-  echo "sudo apt install ncurses-bin"
+  echo "$install_help_text"
   echo
   exit 255
 fi
-
+exit 255
 if [ ! -d "$PREFIX" ]; then
   echo "WARNING: The selected prefix path at $PREFIX does not exist."
   echo
