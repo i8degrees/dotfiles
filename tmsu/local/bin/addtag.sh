2d5698fc5ff1b8b359aaa75eed3f74bbdc341c4d
Jeffrey Carpenter <1329364+i8degrees@users.noreply.github.com>
2024-12-24 01:37:50 -0600

    Add tmsu package

---
+++ tmsu/local/bin/addtag.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# TMSU_DB=$HOME/Pictures/.tmsu/db
+
+function log_debug() {
+  if [[ $DEBUG -eq 1 ]]; then
+    echo "DEBUG(jeff): $@"
+  fi
+}
+
+function log_info() {
+  echo "INFO(jeff): $@"
+}
+
+function run_command() {
+  log_debug $@
+  exec $@
+}
+
+ARGS=()
+ARG_INDEX=1
+for (( i=1; i <= "$#"; i++ )); do
+  ARG_INDEX=$i
+  ARGS+=" ${!i}"
+
+  log_debug "${ARG_INDEX}"
+  log_debug "${ARGS}"
+done
+
+if [[ $ARG_INDEX -le 1 ]]; then
+  log_info tmsu tag $ARGS
+  log_info "tmsu requires at least one tag"
+  exit 255
+fi
+
+# tmsu tag <filename> <tags>
+run_command "tmsu tag $ARGS"
+
+exit 0
