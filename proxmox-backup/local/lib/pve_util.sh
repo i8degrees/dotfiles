82928f65597367cf586c3b8588360d0ff666df73
Jeffrey Carpenter <1329364+i8degrees@users.noreply.github.com>
2024-12-28 11:54:35 -0600

    Add switch for modifying the file detection method
    + PBS v8.3 has announced that the metadata file detection mode is now
    production ready.
    
    + pbs1.password.dist: Add FILE_DETECTION_MODE env variable with the default
    as `legacy`. This allows us to migrate each client individually.

--- proxmox-backup/local/lib/pve_util.sh
+++ proxmox-backup/local/lib/pve_util.sh
@@ -252,7 +252,7 @@ build_run_cmd() {
   if [ "$TYPE" = "home" ]; then
     # shellcheck disable=SC2206
     ARGS+=("${HOST}_home.pxar:/home" $EXCLUSIONS_LIST $INCLUDES)
-  elif [ "$TYPE" = "system" ] || [ "$TYPE" = "root" ]; then
+  elif [ "$TYPE" = "root" ] || [ "$TYPE" = "system" ]; then
     # shellcheck disable=SC2206
     ARGS+=("${HOST}_root.pxar:/" $EXCLUSIONS_LIST $INCLUDES)
   # else
