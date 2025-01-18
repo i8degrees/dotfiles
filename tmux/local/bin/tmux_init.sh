01d65b63a5a622d3f464bd073f40c89d199b13a2
Jeffrey Carpenter <1329364+i8degrees@users.noreply.github.com>
2025-01-18 15:51:41 -0600

    WIP

--- tmux/local/bin/tmux_init.sh
+++ tmux/local/bin/tmux_init.sh
@@ -1,12 +1,21 @@
-#!/usr/bin/env bash
+#!/bin/sh
+
+[ -n "$DEBUG_TRACE" ] && set -o xtrace
 
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 
-USE_TMUX=1
+DEFAULT_SHELL="/bin/bash"
+ALT_SHELL="/bin/sh"
+DEFAULT_CWD="$HOME"
+TMUX_BIN="$(command -v tmux)"
 
-if [ -n "$USE_TMUX" ]; then
+if [ -x "$TMUX_BIN" ] && [ -n "$TMUX" ]; then
   tmux attach || tmux new -s default -A
 else
-  exec /bin/bash -l #-c 'cd ~/Notes.git'
+  if [ -x "$(command -v $DEFAULT_SHELL)" ]; then
+    "$DEFAULT_SHELL" -i -c "cd $DEFAULT_CWD"
+  else
+    "$ALT_SHELL" -c "cd $DEFAULT_CWD"
+  fi
 fi
 
