85cd38d7f307f9db95a7cb6c605b3b67bb38b3c0
Jeffrey Carpenter <1329364+i8degrees@users.noreply.github.com>
2025-03-11 01:37:17 -0500

    Removal of linux-kernel stow pkg

--- linux-kernel/remove.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# ./remove.sh:jeff
-#
-
-KERNEL_INSTALL_SCRIPT="/etc/kernel/install.d/0-mount-boot-parts.install"
-
-[[ -L "$KERNEL_INSTALL_SCRIPT" ]] && rm "$KERNEL_INSTALL_SCRIPT"
-
