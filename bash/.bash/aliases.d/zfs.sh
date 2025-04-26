c74cc68c3fc782ebd9acb901cf61783fd906c18d
Jeffrey Carpenter <1329364+i8degrees@users.noreply.github.com>
2025-04-26 05:00:58 -0500

    Further cleanup of our new user aliases.d/*

--- bash/.bash/aliases.d/zfs.sh
+++ bash/.bash/aliases.d/zfs.sh
@@ -1,7 +1,43 @@
-#!/bin/sh
+#!/usr/bin/env bash
+#
+# ZFS operations helper script
 #
 
-[ -n "$(command -v zstd)" ] && alias zstd='zstd -T4'
-[ -n "$(command -v zfs)" ] && alias zfs-mv='zfs rename'
-[ -n "$(command -v zfs)" ] && alias zfs-move='zfs mv'
+[ -n "$DEBUG" ] && set -o errexit
+[ -n "$DEBUG_TRACE" ] && set -o xtrace
+
+# DEPRECATED(JEFF): This function is made obsolete by
+# the zstd alias using -T0 to specify an auto-calculated
+# number of threads (same as the function). Please use
+# the zstd alias instead of this function as this 
+# function will be removed in the future.
+#
+# Use the maximum number of threads for zstd
+# (de)-compression.
+#
+# _zstd(...args)
+_zstd() {
+  #num_threads="$(($(nproc)-1))"
+  num_threads="$(($(nproc)))"
+  zstd_args="-T${num_threads}"
+  args=$*
+
+  if [ -x "$(which zstd)" ]; then
+    [ -z "$DEBUG" ] && zstd $zstd_args "$args"
+    [ -n "$DEBUG" ] && echo zstd $zstd_args "$args"
+  else
+    echo "ERROR: Failed to find zstd binary..."
+    echo
+    return 2
+  fi
+}
+
+# IMPORTANT(JEFF): Use the maximum number of threads 
+# detected by zstd for (de)-compression.
+[ -x "$(which zstd)" ] && alias zstd='zstd -T0'
+
+if [ -x "$(which zfs)" ]; then
+  alias zfs-mv='zfs rename'
+  alias zfs-move='zfs mv'
+fi
 
