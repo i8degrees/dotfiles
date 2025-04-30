f6159d66dcd0a2614809f2df2b58ee01857f4e9e
Jeffrey Carpenter <1329364+i8degrees@users.noreply.github.com>
2025-04-30 16:03:48 -0500

    Add a default working directory to init tmux with

--- tmux/local/bin/tmux_init.sh
+++ tmux/local/bin/tmux_init.sh
@@ -2,10 +2,15 @@
 
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 
+# TODO(JEFF): I think that I would like to make this an optional parameter
+# where if is is an empty string, for the path not to be fed to the tmux 
+# args.
+CWD="$HOME/Notes.git"
+
 USE_TMUX=1
 
 if [ -n "$USE_TMUX" ]; then
-  tmux attach -tdefault || tmux new -s default -A
+  tmux attach -tdefault -c "$CWD" || tmux new -s default -A -c "$CWD"
 else
   exec /bin/bash -l #-c 'cd ~/Notes.git'
 fi
