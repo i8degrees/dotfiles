aab11f16ef330814d62f3a61488f87da099496a4
Jeffrey Carpenter <1329364+i8degrees@users.noreply.github.com>
2025-05-22 15:33:50 -0500

    Fix remaining shellcheck complaint; we are now sh compliant!
    + If this script is executed when an existing tmux session is
    present, we return what the shell interprets as false (1).
    
    + Revert default CWD to "~/Notes.git"

--- tmux/local/bin/tmux_init.sh
+++ tmux/local/bin/tmux_init.sh
@@ -4,6 +4,14 @@
 #
 #
 
+# shellcheck disable=SC3040
+(set -o pipefail 2> /dev/null) && set -o pipefail
+
+# shellcheck disable=SC3040
+[ -n "$DEBUG" ] && (set -o errexit 2> /dev/null) && set -o errexit
+# shellcheck disable=SC3040
+[ -n "$DEBUG_TRACE" ] && (set -o xtrace 2>/dev/null) && set -o xtrace
+
 init_new_session() {
   session="$1"
   cwd="$1"
@@ -32,35 +40,29 @@ init_existing_session() {
   fi
 }
 
-if [ -n "$BASH" ]; then
-  set -o pipefail
-  [ -n "$DEBUG" ] && set -o errexit
-  [ -n "$DEBUG_TRACE" ] && set -o xtrace
-fi
-
 DEFAULT_SESSION="$1"
 [ -z "$DEFAULT_SESSION" ] && DEFAULT_SESSION="default"
 
 DEFAULT_CWD="$2"
-[ -z "$DEFAULT_CWD" ] && DEFAULT_CWD="$HOME/dotfiles.git"
-
-#if "$(uname -a | tr '[:upper:]' '[:lower:]' | grep -i -e 'Android'>/dev/null)"; then
-#if [ -z "$OSTYPE" ]; then
-  #if ! "$(uname -a|grep -i -e 'Android' >/dev/null)"; then
-    #OSTYPE="linux-android"
-  #fi
-#fi
+[ -z "$DEFAULT_CWD" ] && DEFAULT_CWD="$HOME/Notes.git"
 
 if [ ! -x "$(which tmux)" ]; then
   echo "WARN: Failed to find tmux..."
   echo
+  # NOTE(JEFF): Upon the first failure to find tmux, we pray that this last
+  # ditch try may work -- this only applies to when we are using Termux under
+  # Android OS.
   if [ -n "$TBIN" ] && [ -n "$THOME" ]; then
-    # echo "android"
     PATH="$TBIN:$THOME/bin:$THOME/local/bin:$PATH"
+  else
+    # NOTE(JEFF): This is where we would try another method to find tmux...
+    false
   fi
 fi
 
-
-init_existing_session "${DEFAULT_SESSION}" "${CWD}" ||
-init_new_session "${DEFAULT_SESSION}" "${CWD}"
-
+if [ -n "$TMUX" ]; then
+  return 1
+else
+  init_existing_session "${DEFAULT_SESSION}" "${CWD}" ||
+  init_new_session "${DEFAULT_SESSION}" "${CWD}"
+fi
