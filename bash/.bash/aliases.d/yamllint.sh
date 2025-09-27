22502c6997194a480c6984fb7cf6379528a027f1
Jeffrey Carpenter <1329364+i8degrees@users.noreply.github.com>
2025-09-27 08:27:54 -0500

    OMG! Could I have really fixed the exists_exe func???
    - `bash/.bash/lib:exists_exe`: I am still unable to believe it, but
    apparently just by adding an echo before we return (upon success) is
    enough to fix a long standing bug! Let's find out...

--- bash/.bash/aliases.d/yamllint.sh
+++ bash/.bash/aliases.d/yamllint.sh
@@ -4,4 +4,4 @@
 
 [ -e "$HOME/.bash/lib" ] && . "$HOME/.bash/lib"
 
-[ -x "$(exists_exe yamllint)" ] && alias yamllint='yamllint --no-warnings'
+[ "$(exists_exe yamllint)" ] && alias yamllint='yamllint --no-warnings'
