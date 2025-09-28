fd831bb3dd2139ac6acb7233cd7e589d3a391e1f
Jeffrey Carpenter <1329364+i8degrees@users.noreply.github.com>
2025-09-28 14:37:09 -0500

    Comment execution lines just in case

--- termux-boot/.termux/boot/example.sh
+++ termux-boot/.termux/boot/example.sh
@@ -1,9 +1,5 @@
 #!/data/data/com.termux/files/usr/bin/sh
-#
-# TODO(JEFF): Verify that this script is not
-# executed at startup; I only expect scripts
-# that are marked executable to be parsed.
 
-termux-wake-lock
-sshd
+#termux-wake-lock
+#sshd
 
