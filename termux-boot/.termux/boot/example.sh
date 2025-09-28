eeb932ac6dd80cccfc2505677e81e673d2fd06d0
Jeffrey Carpenter <1329364+i8degrees@users.noreply.github.com>
2025-09-28 08:08:19 -0500

    Initial commit of termux-boot stow pkg

---
+++ termux-boot/.termux/boot/example.sh
@@ -0,0 +1,9 @@
+#!/data/data/com.termux/files/usr/bin/sh
+#
+# TODO(JEFF): Verify that this script is not
+# executed at startup; I only expect scripts
+# that are marked executable to be parsed.
+
+termux-wake-lock
+sshd
+
