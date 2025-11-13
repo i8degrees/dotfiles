21c5dfe63c64e7689829770e37b05b0a3e28c5fa
Jeffrey Carpenter <1329364+i8degrees@users.noreply.github.com>
2025-11-13 00:46:19 -0600

    Initial commit; automation login script concerning audio
    Refactor mpd stow package for better multi-platform usage
    
    WARNING(JEFF): This is a breaking change for existing users!
    
    The changes made today are with the assumption that we are
    setting up another MPD instance on another virtual machine
    that will *never* be online at the same time as another.
    
    + INSTALL.stow
    + .stow-local-ignore
    M conf.d/site.conf -> conf.d/site.conf.dist
        - the existing setup conf is now at `conf.d/site.conf.dist`
    M mpd.conf
        - Add site.conf include
    M conf.d/outputs.conf
        - Disabled pipewire conf; the rationale for this is because not
        every installation is going to have this audio library linked
        with mpd executables

---
+++ pulseaudio-for-gaming/local/bin/audio_setup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+# Create virtual multi-output "surround sound" at login
+#
+# SEE ALSO
+# 1. linux `paprefs` package; this is what I would
+# ordinary use -- this is not an option here.
+#
+
+pactl load-module module-combine-sink sink_name=combined_monitor_sink slaves=alsa_output.pci-0000_01_00.1.hdmi-stereo-extra3,alsa_output.pci-0000_03_00.0.analog-stereo \
+    channels=2 channel_map=front-left,front-right
+
+pactl set-default-sink combined_monitor_sink
