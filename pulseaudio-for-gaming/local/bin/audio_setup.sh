c408c90b8f5c033fc3ac1fbcc8483e585150a370
Jeffrey Carpenter <1329364+i8degrees@users.noreply.github.com>
2025-12-11 16:14:51 -0600

    Add defaults.pa

--- pulseaudio-for-gaming/local/bin/audio_setup.sh
+++ pulseaudio-for-gaming/local/bin/audio_setup.sh
@@ -7,7 +7,10 @@
 # ordinary use -- this is not an option here.
 #
 
+exit 0
+# DEPRECATED(JEFF): This is now tried in ~/.config/pulse/defaults.pa
 pactl load-module module-combine-sink sink_name=combined_monitor_sink slaves=alsa_output.pci-0000_01_00.1.hdmi-stereo-extra3,alsa_output.pci-0000_03_00.0.analog-stereo \
     channels=2 channel_map=front-left,front-right
 
 pactl set-default-sink combined_monitor_sink
+
