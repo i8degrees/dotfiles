afba050ed876c37c6e4675d16b15f630be940a2f
Jeffrey Carpenter <1329364+i8degrees@users.noreply.github.com>
2025-12-14 09:19:55 -0600

    Additional TERMUX_USER env

--- bash/.bash/aliases.d/termux.sh
+++ bash/.bash/aliases.d/termux.sh
@@ -15,13 +15,32 @@ if [[ ! "$OSTYPE" =~ 'linux-android' ]]; then
   return;
 fi
 
+# TODO(JEFF): Add check for the specific [magisk
+# module][1] we depend on for this path being present
+#
+# [1]: https://github.com/Magisk-Modules-Alt-Repo/custom-certificate-authorities.git
 if [ -d $TERMUX_SYSTEM_CA ]; then
-  TERMUX_SYSTEM_CA=/data/misc/user/0/cacerts-custom
+  TERMUX_SYSTEM_CA="/data/misc/user/0/cacerts-custom"
   export TERMUX_SYSTEM_CA
 fi
 
 if [ -d $TERMUX_USER_CA ]; then
-  TERMUX_USER_CA=$HOME/.certs
+  TERMUX_USER_CA=$THOME/.certs
   export TERMUX_USER_CA
 fi
+
+# TODO(JEFF): Add function for doing this check in a loop?
+# TODO(JEFF): Check existance
+TERMUX_USER_NOTES=$THOME/notes.git
+TERMUX_USER_INV=$THOME/fs1_inv.git
+TERMUX_USER_DOTFILES=$THOME/dotfiles
+
+if [ -d $TERMUX_USER_DOTFILES ]; then
+  TERMUX_USER_DOTFILES=$THOME/dotfiles
+  export TERMUX_USER_DOTFILES
+fi
+
+# TODO(JEFF): Introduce PATH file with our common
+# paths for working within Android w/ Termux, i.e.:
+# ~/.bash/path.d
 
