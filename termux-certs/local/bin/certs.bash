4757202bacfcf0783ac595e9b1fc36968b956fbc
Jeffrey Carpenter <1329364+i8degrees@users.noreply.github.com>
2025-12-21 13:50:20 -0600

    Enhance copy_file func && add algorithm for wrapping up

--- termux-certs/local/bin/certs.bash
+++ termux-certs/local/bin/certs.bash
@@ -6,11 +6,19 @@
 # Helper script for management of my privately
 # ran PKI infra at acme.fs1.home.
 #
+# NOTE(JEFF): This package requires several stow
+# packages from my [dotfiles][dotfiles.git]
+# to be setup: bash, termux, termux-certs.
+# Additionally, you will need stow (Perl), git
+# and git-lfs binaries installed via Termux or
+# so.
+#
 # TODO(JEFF): Extend this script to be usable
 # on any (non-Windows) platform, such as our
 # servers (Linux) and lastly, our desktop systems 
 # (MacOS, Linux).
 #
+# [dotfiles.git]: https://github.com/i8degrees/dotfiles.git
 
 [ -r "$HOME/.bash/lib" ] &&
 . "$HOME/.bash/lib"
@@ -36,6 +44,8 @@ link_path() {
   ln -sf "$prefix" "${dest_path}"
 }
 
+# TODO(JEFF): Implement the use of cp when rsync is not found
+#
 # Copy a file from a given source path to its specified 
 # path. Said paths must both be valid (POSIX) filesystem paths
 # as defined by the operating system.
@@ -57,7 +67,15 @@ copy_file() {
   default_params=("-avPu")
 
   # TODO(JEFF): Implement the use of cp when rsync is not found
-  rsync "${default_params[$*]}" "${prefix}" "${dest_path}"
+  #
+  # WARNING(JEFF): This call fails under an Android shell due to
+  # the lack of shell init for the root superuser -- unless
+  # the TBIN value is present during sudo request.
+  if [ -n "$TBIN" ] && [ -d "$TBIN" ]; then
+    "$TBIN/rsync" "${default_params[$*]}" "${prefix}" "${dest_path}"
+  else
+    rsync "${default_params[$*]}" "${prefix}" "${dest_path}"
+  fi
 }
 
 #CWD="$2"
@@ -77,9 +95,9 @@ NUM_FILES=$(ls -1 "${PREFIX}" | wc -l)
 for filename in $(ls -1 "${PREFIX}"); do
   #echo "${filename}"
   dest_filename=$(append_extension ".crt" "${TERMUX_USER_CA}"/"${filename}")
+  echo $dest_filename
   # TODO(JEFF): Add error handling here and subtract
   # unsuccessful links from the NUM_FILES tracking var.
-  # 
   link_path \
     "${CWD}/pem/${filename}" \
     "${CWD}/${dest_filename}"
@@ -88,6 +106,8 @@ done
 if [ "$NUM_FILES" -ge 1 ]; then
   echo "INFO: Linked ${NUM_FILES} file(s) to ${CWD}."
 fi
+
+exit 0
 
 DEST_CA_PATH=
 if [ -n "$TERMUX_SYSTEM_CA" ]; then
@@ -104,6 +124,20 @@ if [ ! -d "$TERMUX_SYSTEM_CA" ]; then
   exit 2
 fi
 
+# TODO(JEFF): We must diff DER encoded certificates from PEM encoding,
+# thus we shall use `file -i $filename | grep charset=binary` in order
+# to do so.
+#
+# 1. (if "binary") openssl x509 -inform DER -outform PEM -in example.crt -out example.crt
+# 2. (else) openssl x509 -noout -text -fingerprint -in example.crt >> example.crt
+# 3. The correct format for the resulting cert is the PEM base64 encoding
+# followed by the human-readable ASCII text below it.
+# 4. mv -v example.crt "$(openssl x509 -subject_hash_old -noout -in example.crt)".0
+# 'example.crt' -> '829893ef.0'
+# 5. cp -v *.0 $TERMUX_SYSTEM_CA/cacerts-custom
+# 6. print to console requesting that the user restarts the device.
+# 7. Profit (!) $$$
+#
 # TODO(JEFF): We must add OS detection around this area
 # in order to prevent execution of Android specific steps;
 # I am thinking that we could split these tasks up into
