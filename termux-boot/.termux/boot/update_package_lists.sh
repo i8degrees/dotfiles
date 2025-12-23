620da4c2bf48b6433c46a9209dd0510dad7f7b74
Jeffrey Carpenter <1329364+i8degrees@users.noreply.github.com>
2025-12-22 20:49:20 -0600

    Fix script pathing issue (we must init env)
    The env `TERMUX_USER_NOTES` was not being defined
    inside this script and thus leading to the invalid
    destination path error.
    
    - Update comments
    - Add `update_packages` function

--- termux-boot/.termux/boot/update_package_lists.sh
+++ termux-boot/.termux/boot/update_package_lists.sh
@@ -1,23 +1,74 @@
-#!/bin/sh
+#!/data/data/com.termux/files/usr/bin/bash
 # update_package_lists.sh:jeff
 #
 # Store a human readable copy of our installed
-# packages via Termux on bootup -- before
-# user auth
+# packages via Termux on bootup -- after user
+# authentication
 #
-# shellcheck shell=dash
+# shellcheck shell=bash
+#
+# TODO(JEFF): Review dpkg && apt notes for more
+# an improved implementation of our package update
+# function; de-duplication and other useful 
+# manipulations.
 
-# FIXME(JEFF): Fix paths
+# TODO(JEFF): Once we have a better grip on the 
+# Termux init scripts, we ought to be able to replace
+# the sourcing of ~/.aliases bits with a lightweight
+# variant that does not end up doing a full shell env
+# init.
 
-DEST_PREFIX="${TERMUX_USER_NOTES}/jeff_pixel4a5g"
+update_packages_list() {
+  dpkg_args=("-l")
+
+  dest_path="$1"
+  if [ -z "$dest_path" ]; then
+    echo
+    echo
+    return 2
+  fi
+
+  dpkg "${dpkg_args[@]}" > "${dest_path}"
+}
+
+if [ -n "$THOME" ]; then
+  echo "INFO: THOME env has been initialized."
+  echo
+  [ -r "$THOME/.aliases" ] &&
+    . "$THOME/.aliases"
+else
+  echo "INFO: THOME env has not been initialized."
+  echo
+  [ -r "${HOME}/.aliases" ] &&
+    . "${HOME}/.aliases"
+fi
+
+if [ -z "$TERMUX_USER_NOTES" ]; then
+  echo "CRITICAL: TERMUX_USER_NOTES env has not been initialized."
+  echo
+  exit 22
+fi
+
 #if [ ! exists_path "$DEST_PREFIX" ]; then
-if [ ! -d $DEST_PREFIX ]; then
+DEST_PREFIX="${TERMUX_USER_NOTES}/jeff_pixel4a5g"
+if [ ! -d "$DEST_PREFIX" ]; then
+  # NOTE(JEFF): If this path does not exist, chances are
+  # that the git repo does not yet exist, or we have
+  # renamed the sub-directory -- jeff_pixel4a5g --
+  # and forgotten all about this script!
+  if [ -n "$DEBUG" ]; then
+    echo "DEBUG:" "${TERMUX_USER_NOTES}"
+    echo
+    echo "DEBUG:" "${DEST_PREFIX}"
+    echo
+  fi
+
   echo "CRITICAL: Destination path does not exist - exiting."
   echo
   exit 2
 fi
 
-# TODO(JEFF): Review dpkg && apt notes for more
-# features, i.e. de-duplication of package lists
-dpkg -l > \
-  "${DEST_PREFIX}/installed_packages.md"
+update_packages_list "${DEST_PREFIX}/installed_packages.md"
+
+exit 0
+
