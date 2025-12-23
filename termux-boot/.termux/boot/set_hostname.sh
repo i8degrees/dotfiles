32303ed568d75cf1872ceaff573da2c3c81b7038
Jeffrey Carpenter <1329364+i8degrees@users.noreply.github.com>
2025-12-22 20:45:25 -0600

    Fix script issues

--- termux-boot/.termux/boot/set_hostname.sh
+++ termux-boot/.termux/boot/set_hostname.sh
@@ -1,14 +1,17 @@
-#!/bin/sh
+#!/data/data/com.termux/files/usr/bin/sh
 # set_hostname.sh:jeff
 #
 # Set the device hostname (Termux on Android)
 #
 
+[ -n "$DEBUG_TRACE" ] &&
+  set -o xtrace
+
 TERMUX_HOSTNAME_FILE=/system/etc/hostname
 
-if [ -f "$TERMUX_HOSTNAME_FILE" ]; then
+if [ -r "$TERMUX_HOSTNAME_FILE" ]; then
   TERMUX_HOSTNAME=$(cat "$TERMUX_HOSTNAME_FILE")
-  hostname -F "$TERMUX_HOSTNAME"
+  sudo hostname -F "$TERMUX_HOSTNAME_FILE"
   export TERMUX_HOSTNAME
 fi
 
