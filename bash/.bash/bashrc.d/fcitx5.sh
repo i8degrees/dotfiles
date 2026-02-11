c5ce7e0abffa3b5ea6418575e28617ed6f715e4d
Jeffrey Carpenter <1329364+i8degrees@users.noreply.github.com>
2026-02-11 02:13:40 -0600

    Initial commit
    Re-run `stow -Rv bash` to activate your local `~/.bash/bashrc.d` path

---
+++ bash/.bash/bashrc.d/fcitx5.sh
@@ -0,0 +1,40 @@
+# ~/.bash/bashrc.d/fcitx5.sh:jeff
+#
+# shellcheck shell=bash
+#
+# Executed by bash(1) shell at
+# the time of sourcing ~/.bashrc
+#
+
+# fcitx5 env
+#
+# 1. https://fcitx-im.org/wiki/Using_Fcitx_5_on_Wayland/en#KDE_Plasma
+#
+# ?? TODO(JEFF): use im-config?
+if ! exists_exe fcitx5 &>/dev/null; then
+  if [ -n "$DEBUG" ]; then
+    echo "fcitx5 is not installed"
+    echo
+  fi
+
+  return 1
+fi
+
+# shellcheck disable=SC2034
+[ "$XDG_SESSION_TYPE" = "wayland" ] &&
+  XMODIFIERS=@im=fcitx
+
+if [ -n "$GTK_IM_MODULE" ]; then
+  # ?? TODO(JEFF): [ -n "$DEBUG" ] &&
+  echo "ERROR: GTK_IM_MODULE was already set -- unsetting this now."
+  echo
+  unset GTK_IM_MODULE
+fi
+
+if [ -n "$QT_IM_MODULE" ]; then
+  # ?? TODO(JEFF): [ -n "$DEBUG" ] &&
+  echo "ERROR: QT_IM_MODULE was already set -- unsetting this now."
+  echo
+  unset QT_IM_MODULE
+fi
+
