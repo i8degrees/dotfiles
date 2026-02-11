5e4896c1f154f63ed2c1ec423c9b9445b360dbbe
Jeffrey Carpenter <1329364+i8degrees@users.noreply.github.com>
2026-02-11 14:18:49 -0600

    Fix shellcheck warn && minor clean up of stdout

--- bash/.bash/bashrc.d/pandoc.sh
+++ bash/.bash/bashrc.d/pandoc.sh
@@ -7,13 +7,20 @@
 [ -n "$DEBUG" ] && set -o errexit
 [ -n "$DEBUG_TRACE" ] && set -o xtrace
 
-[ -r "$HOME/.bash/lib" ] && . "$HOME/.bash/lib"
+# shellcheck disable=SC1091
+[ -r "$HOME/.bash/lib" ] &&
+  . "$HOME/.bash/lib"
 
 # pandoc env
 #
 # Generate BASH completions for Pandoc
 if exists_exe pandoc &>/dev/null &&
   pandoc --bash-completion &>/dev/null; then
+    echo "INFO: Initializing pandoc BASH completions."
+    echo
     eval "$(pandoc --bash-completion)"
+else
+  echo "WARN: pandoc executable is not available."
+  echo
 fi
 
